c9fabfda3880927a18f87d101fef3b2bbaeedb72
HorsFD <gustavosteilein@gmail.com>
2026-08-26 14:25:57 -0300

    feat(ui): consolida design system premium

---
+++ src/Detara.Web/Components/Shared/DesignSystem/DesignSystemTones.cs
@@ -0,0 +1,25 @@
+namespace Detara.Web.Components.Shared.DesignSystem;
+
+public enum DetaraMetricTone
+{
+    Neutral,
+    Positive,
+    Warning,
+    Info
+}
+
+public enum DetaraStatusTone
+{
+    Neutral,
+    Positive,
+    Warning,
+    Info,
+    Critical
+}
+
+public enum DetaraSkeletonVariant
+{
+    Cards,
+    Table,
+    Detail
+}
